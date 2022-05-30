39a4f14d4077180335931b4b10c7ebab3e14c2c6
Dayana Tito <Dayanatitochoque@hotmail.com>
2022-05-30 13:52:00 -0300

    Adicionando Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/estaticos/footer/Footer";
-=======
-import React from 'react';
->>>>>>> e61a7e8eecd8c0167304ff1f83ace87675d1068e
+import Navbar from "./components/estaticos/navbar/Navbar";
 
 function App() {
   return (
     <Router>
       <Navbar />
-<<<<<<< HEAD
-      <div style={{ minHeight: "100vh" }}>
-        <Routes>
-          {" "}
-          // Antigo Switch
-=======
       <div style={{ minHeight: '100vh' }}>
         <Routes>
->>>>>>> e61a7e8eecd8c0167304ff1f83ace87675d1068e
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
@@ -29,11 +19,7 @@ function App() {
       </div>
       <Footer />
     </Router>
-<<<<<<< HEAD
   );
-=======
-  )
->>>>>>> e61a7e8eecd8c0167304ff1f83ace87675d1068e
 }
 
 export default App;
