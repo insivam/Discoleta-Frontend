4ebcfde41f00d889c3250577cd295343c49d110b
milenaFO <mmilenaf@gmail.com>
2022-05-30 13:54:49 -0300

    Adicionando Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home";
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <div style={{ minHeight: '100vh' }}>
+      <Routes>
+      <Route path="/home" element={<Home />} />
+      </Routes>
+      </div>
+      </Router>
   );
 }
 
